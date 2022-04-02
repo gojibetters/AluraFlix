89f1516f2954079a5fb674c496d6477805dbf21e
goji <patezbr@gmail.com>
2022-04-01 23:28:03 -0300

    update tag <h1>

--- script.js
+++ script.js
@@ -26,29 +26,28 @@ function adicionarLista(filmeFavorito) {
 function mostrarFilme(lista) {
     const elementoFilme = document.getElementById("listaFilmes");
     const mostrartagFilme = document.getElementById("MostrarFilme");
-    mostrartagFilme.innerHTML = "<h1> Filmes </h1>";
+    mostrartagFilme.innerHTML = "<h1>Filmes</h1>";
+    if (!lista.length) { mostrartagFilme.innerHTML = ""; }
+
     elementoFilme.innerHTML = lista;
 }
 
 function verifyExtensionRemove() {
     const FilmeFavorito = document.getElementById("filme").value;
     const searchTerm = listaFilmes.indexOf(FilmeFavorito);
+
     if (searchTerm == -1) {
         window.alert("Nenhum filme foi informado ou URL inválido.");
-
     } else {
         if (FilmeFavorito.endsWith(".jpg") || FilmeFavorito.endsWith(".JPG") ||
             FilmeFavorito.endsWith(".JPEG") || FilmeFavorito.endsWith(".JPEG")) {
             listaFilmes.splice(searchTerm, 1);
             listaimgFilmes.splice(searchTerm, 1);
-
         } else {
-            //console.error("URL INCORRETO");
             window.alert("URL INCORRETO.")
         }
     }
 
-    //TODO: 1. LIMPAR A TAG H1 APÓS TODOS OS FILMES SEREM REMOVIDOS
     //      2. APRESENTAR O NOME DOS FILMES ACIMA DA CARTAZ
 
     document.getElementById("filme").value = "";
